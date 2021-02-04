827e491d39ed700cfc2fbb1298fd4ff12408365a
Monzer Nasr <Monzer.Nasr@axceligent.com>
2021-02-04 13:12:35 +0800

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyOverlayService } from './my-overlay-container.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [MyOverlayService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/my-overlay-container.service.ts
@@ -0,0 +1,20 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Directive, Injectable, OnDestroy } from '@angular/core';
+
+@Directive()
+@Injectable()
+export class MyOverlayService extends OverlayContainer implements OnDestroy {
+  protected _createContainer(): void {
+    super._createContainer();
+    if (!this._containerElement) {
+      return;
+    }
+    const parent = document.querySelector('axc-ppms-pages') || document.body;
+    parent.appendChild(this._containerElement);
+  }
+
+  ngOnDestroy() {
+    super.ngOnDestroy();
+    this._containerElement = null;
+  }
+}
